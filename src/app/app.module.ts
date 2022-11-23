309a125625ceea986a2c4fac3ce18c3c83ebd297
mohammadshaban01 <43331502+mohammadshaban01@users.noreply.github.com>
2022-11-23 14:04:22 +0530

    last con-fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,18 @@ import { DevelopersRegisterComponent } from './components/pages/developers-regis
 import { CompaniesRegisterComponent } from './components/pages/companies-register/companies-register.component';
 import { NotFoundComponent } from './components/pages/not-found/not-found.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+
 
 @NgModule({
   declarations: [
@@ -27,7 +39,21 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     CompaniesRegisterComponent,
     NotFoundComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FlexLayoutModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatCardModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    FlexLayoutModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
